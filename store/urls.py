d70d83f7c7ff30bebcbd6172c669b3d9ec137701
Duc Thinh <doanducthinh2004@gmail.com>
2026-02-01 00:26:00 +0700

    Trang chi tiet san pham va chinh sua trang danh sach

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,11 @@
+# store/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Đường dẫn trang chủ (Danh sách sản phẩm)
     path('', views.product_list, name='product_list'),
+
+    # Đường dẫn trang chi tiết (Ví dụ: /product/1/)
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]

602585609ded98aa2c17845b0726602f1f240b38
Nguyễn Tiến Phát <phatnguyen031225@gmail.com>
2026-02-01 11:52:43 +0700

    Hoan thanh chuc nang tim cua hang gan nhat voi PostGIS

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
 
     # Đường dẫn trang chi tiết (Ví dụ: /product/1/)
     path('product/<int:pk>/', product_views.product_detail, name='product_detail'),
+
+    path('store-locator/', store_views.store_locator, name='store_locator'),
+    path('api/find-nearest/', store_views.api_find_nearest_store, name='api_find_nearest'),
+
 ]
